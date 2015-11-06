6e5d22d0f959419c991cf9b73a9f3ea22bc21e08
jasmine_song <jasmsong@gmail.com>
2015-11-06 14:41:53 -0500

    init

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+var app=angular.module("NoteWrangler",["ngRoute"])
+                                .config(['$routeProvider',function($routeProvider){
+    $routeProvider.when('/notes',{
+    templateUrl: 'templates/pages/notes/index.html '
+  });
+  
+  
+}]);       
